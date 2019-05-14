f50a1f3d4bdfd9202110bab22a3e783dc0d10036
kueikuei <jasonchen@richitech.com.tw>
2019-05-14 15:00:46 +0800

    🐛 fix: 改回lonebot package

--- app.js
+++ app.js
@@ -27,8 +27,8 @@
 // });
 
 
-
-const line = require('@line/bot-sdk');
+var linebot = require('linebot');
+// const line = require('@line/bot-sdk');
 const express = require('express');
 // const lineConfig = {
 //   channelAccessToken: process.env.Channel_access_token,
@@ -39,40 +39,29 @@ const lineConfig = {
     channelAccessToken: 'qdzy8O4OluZkTmqpAO/LzvNOnZQ5XNHm7G2XWj3eFGVoDeCMXO4kptPS3IWFc1S/g0wxDLZzRaVPtN4HTfvOgq+iJImuu3yIc7kHNB030JWLsGg++pT4K5GZgsAHbzr3CIH23OfOwcu4JzpTMSPakQdB04t89/1O/w1cDnyilFU=',
     channelSecret:  '3696ca91a8467bda329372c3fd5f3b13'
 }
-const client = new line.Client(lineConfig);
-const app = express();
-
-app.listen(3000, function() {
-    console.log('App now running on port', this.address().port);
-});
-
-app.post('/', line.middleware(lineConfig), function(req, res) {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then(function(result) {
-            res.json(result);
-        });
-});
-
-
-function handleEvent(event) {
-    switch (event.type) {
-      case 'join':
-      case 'follow':
-        return client.replyMessage(event.replyToken, {
-          type: 'text',
-          text: '你好請問我們認識嗎?'
-        });   
-      case 'message':
-        switch (event.message.type) {
-          case 'text':
-            return client.replyMessage(event.replyToken, {
-              type: 'text',
-              text: (event.message.text+'~*')
-            });
-        }
+//這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
+bot.on('message', function(event) {
+    if (event.message.type = 'text') {
+      var msg = event.message.text;
+    //收到文字訊息時，直接把收到的訊息傳回去
+      event.reply(msg).then(function(data) {
+        // 傳送訊息成功時，可在此寫程式碼 
+        console.log(msg);
+      }).catch(function(error) {
+        // 傳送訊息失敗時，可在此寫程式碼 
+        console.log('錯誤產生，錯誤碼：'+error);
+      });
     }
-  }
+  });
+  
+  const app = express();
+  const linebotParser = bot.parser();
+  app.post('/', linebotParser);
+  
+  var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log('目前的port是', port);
+  });
 
 
 
