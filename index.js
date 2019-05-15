720d07c7a1aecdbfc14256b93ef432ab00e91b7e
kueikuei <jasonchen@richitech.com.tw>
2019-05-15 13:25:09 +0800

    \u2705 Add: test

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ bot.on('message', function(event) {
   if (event.message.type = 'text') {
     var msg = event.message.text;
   //收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(msg).then(function(data) {
+    event.reply('test').then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg+'test');
-      console.log('test')
+      console.log(msg);
     }).catch(function(error) {
       // 傳送訊息失敗時，可在此寫程式碼 
       console.log('錯誤產生，錯誤碼：'+error);
@@ -25,7 +24,7 @@ bot.on('message', function(event) {
   
 const app = express();
 const linebotParser = bot.parser();
-app.post('/', 'test');
+app.post('/', linebotParser);
 
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
