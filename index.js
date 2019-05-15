f26332a96f48cd2b1f8fdfd3d928ea8823ea8c50
kueikuei <jasonchen@richitech.com.tw>
2019-05-15 13:12:30 +0800

    ✅ Add: test

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ bot.on('message', function(event) {
   
 const app = express();
 const linebotParser = bot.parser();
-app.post('/', linebotParser);
+app.post('/', linebotParser+'test');
 
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
