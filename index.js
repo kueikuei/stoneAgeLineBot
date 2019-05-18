47038af5938a15518ea3d2fc7de35d926df49aec
kueikuei <jasonchen@richitech.com.tw>
2019-05-18 23:20:18 +0800

    ✨ Add: 寫檔 - key in 新關鍵字、內容給機器人

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ else{
 bot.on('message', function(event) {
 
   if (event.message.type = 'text') {
-    console.log('test',event.message.text)
+    console.log('test',event.message.text[0])
 
     // 關鍵字回覆
     rtnMsg(data[0][event.message.text]);
