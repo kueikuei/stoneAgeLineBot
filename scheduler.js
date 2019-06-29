884e4bae67e1ea2af242e56a40ca211ed72a85c4
kueikuei <jasonchen@richitech.com.tw>
2019-06-29 17:07:42 +0800

    \u2728 Add: 新增排程程式

--- scheduler.js
+++ scheduler.js
@@ -10,8 +10,6 @@ var allIDs = [
     'C9b8eec334a494490e84e21a736ebd196', //A
     'Cac9b057386d3a2b7c7c795e1db0b1b98', // B
     'C516266dd76bfcd49c07f2f61002fd989', // C
-    // 'U3b90812bccb505e9a03722a0a772c894', // kuei
-    // 'C4dbfa6899586434e80ec17ed161598ea'
   ]
   
 
